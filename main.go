4557ee75b943216e87cc67f405954fdf3a95c688
Jeff Kramer <jeffkramr@gmail.com>
2013-09-19 01:16:16 -0400

    Just added a bit of code to use all available cores.  Setting up for concurrency.
    Big question remains: Is it faster/better to block on channel for all agents to send in requests, or is it better for a BufferedChannel to be set up to absorb all requests, then read them out of the BufferedChannel.
    
    The first simplifies the code(?), the second provides better control(?).

--- main.go
+++ main.go
@@ -3,8 +3,15 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 )
 
 func main() {
 	fmt.Println("Hello World!")
+}
+
+//Set up our agent system/world state in here.
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	fmt.Printf("Number of CPUS: %d\n", runtime.NumCPU())
 }
